b52291ccce0f13b43c11b2a6dfe502ccfc161b5f
Jorge Luis <jorgequiguango@outlook.com>
2025-01-26 09:12:25 -0500

    [ADD] System variables

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,13 +2,31 @@ from flask import Flask
 from flask_restful import Api
 from flaskr.routes import load_routes
 from flaskr.database import db
+import os
 
 
 def create_app():
     app = Flask(__name__)
     api = Api(app)
 
-    app.config.from_pyfile('config.cfg')
+    env = os.environ.get('ENV', 'development')
+    print(f'ENV: {env}')
+
+    if env == 'development':
+        app.config.from_pyfile('config.cfg')
+    else:
+        user = os.environ['USER']
+        password = os.environ['PASSWORD']
+        host = os.environ['HOST']
+        port = os.environ['PORT']
+        database = os.environ['DATABASE']
+        database_url = (
+            f'postgresql+psycopg://{user}:{password}@{host}:{port}/{database}'
+        )
+        app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_ECHO'] = True
+
     db.init_app(app)
 
     load_routes(api)
---
+++ set_variables.sh
@@ -0,0 +1,6 @@
+export ENV=production
+export HOST=localhost
+export DATABASE=hello
+export USER=hello
+export PASSWORD=h
+export PORT=5432
